f7b7c7c5c9095773ca90fa4736c1b6a34ff6f9d5
Lewis Carhart <lewis@futureon.com>
2024-04-14 22:27:19 +0100

    Better messaging when onboarding is compelte

--- src/app/(home)/organization-setup/components/OnboardingForm.tsx
+++ src/app/(home)/organization-setup/components/OnboardingForm.tsx
@@ -1,13 +1,12 @@
 "use client";
 import * as React from "react";
-
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { redirect } from "next/navigation";
 import { z } from "zod";
 import { api } from "@/trpc/react";
 import {
@@ -27,78 +26,107 @@ const formSchema = z.object({
 
 export function OnboardingForm() {
   const { toast } = useToast();
+  const [isOrgSetup, setIsOrgSetup] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const { mutate, isSuccess, isError, isPending } =
     api.org.setupOrg.useMutation();
+  const { data } = api.org.isOrgSetup.useQuery();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
     mutate(values);
   }
 
   useEffect(() => {
     if (isSuccess) {
+      setIsLoading(true);
+      setIsOrgSetup(true);
       toast({
         title: "Success 🎉",
         description: "Your organization has been successfully set up.",
       });
-
-      redirect("/dashboard");
     }
 
     if (isError) {
+      setIsLoading(false);
       toast({
         title: "Error ⛔",
         description:
           "There was an error creating your organization. Please try again.",
       });
     }
-  }, [isSuccess, isError]);
+  }, [isSuccess, isError, isOrgSetup, data, isLoading]);
 
   return (
     <div>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col space-y-4"
-        >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Organization name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Ex. Acme Inc." {...field} type="text" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="domain"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email domain name</FormLabel>
-                <FormControl>
-                  <Input placeholder="mail.acme.com" {...field} type="text" />
-                </FormControl>
-                <FormDescription>
-                  Important: this should be a domain you own, e.g.
-                  mail.acme.com.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" variant="default" disabled={isPending}>
-            Continue
+      {!isOrgSetup ? (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Organization name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ex. Acme Inc." {...field} type="text" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="domain"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email domain name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="mail.acme.com" {...field} type="text" />
+                  </FormControl>
+                  <FormDescription>
+                    Important: this should be a domain you own, e.g.
+                    mail.acme.com.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" variant="default" disabled={isPending}>
+              Setup organization
+            </Button>
+          </form>
+        </Form>
+      ) : (
+        <div className="flex flex-col">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden="true"
+            data-slot="icon"
+            className="mb-8 w-16 items-center justify-center self-center rounded-full bg-green-500 p-1 text-white ring-8 ring-green-500/30 dark:ring-green-500/50"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m4.5 12.75 6 6 9-13.5"
+            ></path>
+          </svg>
+          <Button asChild className="flex flex-col space-y-4" variant="outline">
+            <Link href="/dashboard">Continue to your dashboard</Link>
           </Button>
-        </form>
-      </Form>
+        </div>
+      )}
     </div>
   );
 }
