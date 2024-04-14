a9914ebe299024faec7504f6cd9af3239d74b50a
Ivan Rybnikov <ivrybn@gmail.com>
2024-04-14 13:56:00 +0300

    sqlite + npm?

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,15 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import Database from "better-sqlite3";
 
-import { env } from "@/env";
 import * as schema from "./schema";
 
-const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN });
+const sqlite = new Database("db/emailthing.sqlite");
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(sqlite, { schema });
+
+// const client = createClient({
+//   url: env.DATABASE_URL,
+//   authToken: env.DATABASE_AUTH_TOKEN,
+// });
+
+// export const db = drizzle(client, { schema });
