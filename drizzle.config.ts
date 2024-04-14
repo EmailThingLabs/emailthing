3e886d6c0aa0a60ae8c6e7d2ee7c2b005100295a
Ivan Rybnikov <ivrybn@gmail.com>
2024-04-14 14:05:03 +0300

    switch the driver

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,11 @@ import { env } from "@/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  driver: "turso",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
-  },
+  driver: "better-sqlite",
+  // dbCredentials: {
+  //   url: env.DATABASE_URL,
+  //   authToken: env.DATABASE_AUTH_TOKEN,
+  // },
   out: "./drizzle",
   tablesFilter: ["emailthing_*"],
 } satisfies Config;
