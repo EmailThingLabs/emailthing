7d25b349706f176d759a6bb5518b4efd0a5dbd4b
Ivan Rybnikov <ivrybn@gmail.com>
2024-04-15 13:36:36 +0300

    greatest logo of all time

--- src/components/nav/main-nav.tsx
+++ src/components/nav/main-nav.tsx
@@ -3,11 +3,13 @@
 import * as React from "react";
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
+import { Send } from "lucide-react";
 
 export function MainNav() {
   return (
     <div className="flex">
       <Link href="/" className="flex items-center">
+        <Send className="mr-2 size-5" strokeWidth={2.3} />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
     </div>
